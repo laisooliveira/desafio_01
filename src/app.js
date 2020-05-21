7a95c8004e47a05fa1d680ef9ef2b6cc9d1a83f1
Lais <lais.aguiar.info@gmail.com>
2020-05-21 18:38:23 -0300

    Desafio 01 - Back-end com Node.js

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository id' });
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateProjectId);
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repo);
+  return response.json(repo);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: "Repository doesn't exists" });
+  }
+
+  repositories[index].title = title;
+  repositories[index].url = url;
+  repositories[index].techs = techs;
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[index].likes
+  };
+  return response.json(repo);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: "Repository doesn't exists" });
+  }
+  repositories.splice(index, 1);
+  return response.status(204).json();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repo => repo.id === id);
+  if (index < 0) {
+    return response.status(400).json({ error: "Repository doesn't exists" });
+  }
+  repositories[index].likes = repositories[index].likes + 1;
+  const { title, url, techs, likes } = repositories[index];
+  const atualizedRepo = {
+    title,
+    url,
+    techs,
+    id,
+    likes,
+  };
+  return response.json(atualizedRepo);
+});
+
+module.exports = app;
